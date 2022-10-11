749a1d58b4952a60a2f9aa7e5a28d6544d774680
zhenya1 <111535920+zhenia1@users.noreply.github.com>
2022-10-11 19:06:12 +0500

    task

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] words(string[] array)
+{
+
+    string[] NewWords = new string[11];
+
+    int count = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            NewWords[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref NewWords, count);
+    return NewWords;
+}
+
+void Print(string[] array)
+{
+    Console.Write("[" + string.Join(", ", array) + "]");
+    Console.WriteLine();
+}
+
+string[] NewArray = { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+
+Print(words(NewArray));
